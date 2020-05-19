3200bce46eebc3fe3175d485473f90c3d6c9011e
Mark Fabrizio <fabrizim@owlwatch.com>
2020-05-19 14:30:40 -0400

    Lighten up the background color

--- single-location.php
+++ single-location.php
@@ -8,7 +8,7 @@ get_header();
 <div id="main-content">
 	<div class="container">
 		<div id="content-area" class="clearfix">
-			<h1 style="text-align: center; padding: 0.5em; background: #d8d8d8;">
+			<h1 style="text-align: center; padding: 0.5em; background: #f2f2f2; border-bottom: 1px solid #d8d8d8;">
 				Locations: <?php the_title(); ?>
 			</h1>
 			<?php
