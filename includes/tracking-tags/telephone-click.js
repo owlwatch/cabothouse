0684c13203f2222ea59a4c0ab95f5a996899c645
Mark Fabrizio <fabrizim@owlwatch.com>
2021-09-09 10:13:45 -0400

    Add tracking-tags

---
+++ includes/tracking-tags/telephone-click.js
@@ -0,0 +1,5 @@
+var axel = Math.random() + "";
+var a = axel * 10000000000000;
+document.write('<iframe src="https://10589887.fls.doubleclick.net/activityi;src=10589887;type=conv90;cat=submi0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
+
+console.log('confirm script was loaded');
