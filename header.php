753370cb0f2c4c3a5f5ec43cdaec4a4799f4b5cc
Mark Fabrizio <fabrizim@owlwatch.com>
2021-09-09 10:08:04 -0400

    Changes for designer Also includes changes made to the theme

--- header.php
+++ header.php
@@ -18,6 +18,7 @@ $header = ob_get_clean();
 
 $svg = file_get_contents( get_stylesheet_directory().'/src/svg/logo-helvetica.svg' );
 
+/*
 // lets replace the logo with the svg
 $header = preg_replace_callback('#<img[^>]+?id="logo"[^>]*?/>#', function($matches) use ($svg){
 	$imgElement = new SimpleXMLElement( $matches[0] );
@@ -39,5 +40,6 @@ $header = preg_replace_callback('#<img[^>]+?id="logo"[^>]*?/>#', function($match
 	}
 	return $svgElement->asXML();
 }, $header);
+*/
 
 echo $header;
--- single-designer.php
+++ single-designer.php
@@ -1,41 +1,25 @@
 <?php
+
 add_filter('body_class', function( $classes ){
 	$classes[] = 'et_full_width_page';
 	return $classes;
 });
 
-get_header();
-// lets grab the layout
-?>
-<div id="main-content">
-    <?php while ( have_posts() ) : the_post(); 
-        $email = get_field( 'email' );
-        ?>
-        <h1 class="screen-reader-text">
-            <?php the_title(); ?>
-        </h1>
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			
-        <div class="et_pb_section et_pb_section_0 et_section_regular">
-				
-				
-				
-				
-                <div class="et_pb_row et_pb_row_0">
-            <div class="et_pb_column et_pb_column_2_5 et_pb_column_0    et_pb_css_mix_blend_mode_passthrough">
-            
-            
-            <div class="et_pb_module et_pb_image et_pb_image_0 et_always_center_on_mobile">
-            
-            
-            <span class="et_pb_image_wrap ">
-                <a href="mailto:<?php echo $email; ?>?bcc=info@cabothouse.com" target="_blank"><?php the_post_thumbnail( ); ?></a>
-            </span>
-        </div><div class="et_pb_module et_pb_text et_pb_text_0 et_pb_bg_layout_light  et_pb_text_align_left">
-            
-            
-            <div class="et_pb_text_inner">
-<p>Interior Designer<br>
+add_filter('the_content', function(){
+    $email = get_field( 'email' );
+    // [et_pb_section fb_built="1" _builder_version="4.10.6" _module_preset="default"][et_pb_row _builder_version="4.10.6" _module_preset="default" column_structure="2_5,3_5"][et_pb_column _builder_version="4.10.6" _module_preset="default" type="2_5"][et_pb_text _builder_version="4.10.6" _module_preset="default" hover_enabled="0" sticky_enabled="0"][/et_pb_text][/et_pb_column][et_pb_column _builder_version="4.10.6" _module_preset="default" type="3_5"][et_pb_text _builder_version="4.10.6" _module_preset="default" hover_enabled="0" sticky_enabled="0"][/et_pb_text][/et_pb_column][/et_pb_row][/et_pb_section]
+    ob_start();
+    ?>
+    [et_pb_section fb_built="1" _builder_version="4.10.6" _module_preset="default"]
+    [et_pb_row _builder_version="4.10.6" _module_preset="default" column_structure="2_5,3_5"]
+    [et_pb_column _builder_version="4.10.6" _module_preset="default" type="2_5"]
+    <div class="et_pb_module et_pb_image et_pb_image_0 et_always_center_on_mobile">
+        <span class="et_pb_image_wrap ">
+            <a href="mailto:<?php echo $email; ?>?bcc=info@cabothouse.com" target="_blank"><?php the_post_thumbnail( ); ?></a>
+        </span>
+    </div>
+    [et_pb_text _builder_version="4.10.6" _module_preset="default" hover_enabled="0" sticky_enabled="0"]
+    <p>Interior Designer<br>
 <strong>
     <?php the_title(); ?>
     <?php 
@@ -57,7 +41,7 @@ $phone = get_field( 'phone' );
 if( $phone ){
     ?>
 <p>Phone<br>
-<a href="tel:<?php echo preg_match('/[^\d]/', $phone); ?>"><?php echo $phone; ?></a>
+<a href="tel:<?php echo preg_replace('/[^\d]/', '', $phone); ?>"><?php echo $phone; ?></a>
     <?php
 }
 if( $email ){
@@ -100,17 +84,14 @@ if( $page ){
     <?php
 }
 ?>
-            </div>
-        </div> <!-- .et_pb_text -->
-        </div> <!-- .et_pb_column --><div class="et_pb_column et_pb_column_3_5 et_pb_column_1    et_pb_css_mix_blend_mode_passthrough">
-            
-        <?php
-        echo "<!--";
-        print_r( get_field( 'portfolio' ) );
-        echo "-->";
+    [/et_pb_text]
+    [/et_pb_column]
+    [et_pb_column _builder_version="4.10.6" _module_preset="default" type="3_5"]
+    [et_pb_text _builder_version="4.10.6" _module_preset="default" hover_enabled="0" sticky_enabled="0"]
+
+    <?php
         $portfolio = get_field( 'portfolio' );
         if( !empty($portfolio) ){
-            ob_start();
             ?>
             [et_pb_slider show_pagination="off" admin_label="Designer Portfolio Photos" _builder_version="3.21.1" module_class="slider-4-3"]
             <?php
@@ -123,19 +104,15 @@ if( $page ){
             ?>
             [/et_pb_slider]
             <?php
-        
-            echo do_shortcode( ob_get_clean() );
         }
         ?>
         
         <div class="et_pb_module et_pb_text et_pb_text_1 et_pb_bg_layout_light  et_pb_text_align_left">
-            
-            
             <div class="et_pb_text_inner">
                 <?php
                 wp_reset_postdata();
                 wp_reset_query();
-                the_content();
+                echo get_post()->post_content;
                 ?>
             </div>
         </div> <!-- .et_pb_text -->
@@ -147,21 +124,64 @@ if( $page ){
             <hr style="border-style: solid; border-width: 1px 0 0; border-color: #ccc;" />
             <h4>Contact <?php echo get_field( 'first_name' ); ?></h4>
             <?php
-            echo do_shortcode('[gravityform id="'.$designer_form_id.'" title="false" description="false" ajax="true"]');
+            echo '[gravityform id="'.$designer_form_id.'" title="false" description="false" ajax="true"]';
         }
             ?>
         </div>
             <?php
         ?>
-        </div> <!-- .et_pb_column -->
-            
-            
-        </div> <!-- .et_pb_row -->
-            
-            
-        </div>
+    [/et_pb_text]
+    [/et_pb_column]
+    [/et_pb_row]
+    [/et_pb_section]
+    <?php
+    $content = ob_get_clean();
+    return preg_replace( '/\n/', '', $content );
+}, 1);
 
+get_header();
+// lets grab the layout
+?>
+<div id="main-content">
+    <?php while ( have_posts() ) : the_post(); 
+        $email = get_field( 'email' );
+        ?>
+        <h1 class="screen-reader-text">
+            <?php the_title(); ?>
+        </h1>
 
+<style>
+.et_pb_slider .et_pb_slide_image {
+	margin-top: 0 !important;
+}
+
+.et_pb_slide {
+	padding: 0;
+}
+
+.et_pb_slide_image img {
+	max-height: inherit !important;
+	width: 100%;
+	height: 100%;
+}
+
+.et_pb_slide_description {
+	display: none !important;
+}
+
+.et_pb_slider .et_pb_slide_0 {
+	background-color: #FFFFFF !important;
+}
+
+.et_pb_bg_layout_light .et-pb-arrow-next, .et_pb_bg_layout_light .et-pb-arrow-prev {
+	color: #FFFFFF !important;
+}	
+.et_pb_empty_slide {
+	height: auto !important;
+}
+</style>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <?php the_content(); ?>
 		</article> <!-- .et_pb_post -->
 
 	<?php endwhile; ?>
