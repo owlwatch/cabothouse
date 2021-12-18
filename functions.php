e43e0dca855e2b4af4a2a82d0e263d464541fc10
Mark Fabrizio <fabrizim@owlwatch.com>
2021-12-17 19:24:03 -0500

    more debugging

--- functions.php
+++ functions.php
@@ -84,6 +84,8 @@ set_error_handler("exception_error_handler");
 add_filter('pre_http_request', function($preempt, $parsed_args, $url){
 	if( strpos($url, home_url()) !== false ){
 		error_log( print_r($url, 1));
+		$e = new Exception();
+		error_log( $e->getTraceAsString() );
 	}
 	return $preempt;
 }, 10, 3);
