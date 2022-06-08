65ce4a1de846f2a727ea529bd9ed0681bb84066a
Mark Fabrizio <fabrizim@owlwatch.com>
2022-06-08 14:21:31 -0400

    rebuild composer files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5a28ce441923e53054c34062452d946b65b67184',
+    'name' => 'cabothf/cabothouse',
+  ),
+  'versions' => 
+  array (
+    'cabothf/cabothouse' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5a28ce441923e53054c34062452d946b65b67184',
+    ),
+  ),
+);
