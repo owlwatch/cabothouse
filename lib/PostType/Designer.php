67969a2264948f5423c90cc0e07431f5f6f4d31a
Mark Fabrizio <fabrizim@owlwatch.com>
2024-06-19 12:38:26 -0400

    move autoplay attributes to the right element

--- lib/PostType/Designer.php
+++ lib/PostType/Designer.php
@@ -146,11 +146,11 @@ if( $page ){
         $portfolio = get_field( 'portfolio' );
         if( !empty($portfolio) ){
             ?>
-[et_pb_slider _builder_version="4.25.2" _module_preset="default" show_pagination="off" height="500px" hover_enabled="0" sticky_enabled="0"]
+[et_pb_slider _builder_version="4.25.2" _module_preset="default" show_pagination="off" height="500px" hover_enabled="0" sticky_enabled="0" auto="on" animation_style="fade" auto_speed="5000"]
             <?php
             foreach( $portfolio as $image){
                 ?>
-[et_pb_slide _builder_version="4.25.2" _module_preset="default" title_text="dining room thumbnail" background_image="<?php echo $image['url'] ?>" background_enable_image="on" hover_enabled="0" sticky_enabled="0" auto="on" animation_style="fade" auto_speed="5000"]
+[et_pb_slide _builder_version="4.25.2" _module_preset="default" title_text="dining room thumbnail" background_image="<?php echo $image['url'] ?>" background_enable_image="on" hover_enabled="0" sticky_enabled="0"]
 [/et_pb_slide]
                 <?php
             }
